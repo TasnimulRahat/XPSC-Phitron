e1e17be00a3f4a4e90a9c73c852efdbfee5aa65b
TasnimulRahat <ridoyhassan2003@gmail.com>
2024-12-06 23:51:13 +0600

    Add files via upload
    Uploaded Week6 Day6

---
+++ Day6/Dull_Operation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        cout << n << " " << n - 1 << endl;
+    }
+
+    return 0;
+}
