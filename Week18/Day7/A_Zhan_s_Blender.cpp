997b7a211d451b6fe05600bc597c7ce60f1b0c59
Tasnimul Hassan Rahat <ridoyhassan2003@gmail.com>
2025-03-08 23:50:24 +0600

    Add files via upload
    Uploaded Week18 Day7

---
+++ Week18/Day7/A_Zhan_s_Blender.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define endl '\n'
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+
+    double n, a, b;
+    cin >> n >> a >> b;
+    a = min(a, b);
+    a = ceil(n / a);
+    int x = a;
+    cout << x << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
