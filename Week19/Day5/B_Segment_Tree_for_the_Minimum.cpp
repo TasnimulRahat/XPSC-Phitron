49d95e7698cf731e3daa6ebad400adbae44deee3
Tasnimul Hassan Rahat <ridoyhassan2003@gmail.com>
2025-03-13 23:38:19 +0600

    Add files via upload
    Uploaded Week19 Day5

---
+++ Week19/Day5/B_Segment_Tree_for_the_Minimum.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define endl '\n'
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+const int maxN = 1e5 + 7;
+ll a[maxN], t[4 * maxN];
+
+void build(int n, int b, int e)
+{
+    if (b == e)
+    {
+        t[n] = a[b];
+        return;
+    }
+
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+
+    t[n] = min(t[l], t[r]);
+}
+
+void update(int n, int b, int e, int i, int v)
+{
+    if (i < b || i > e)
+    {
+        return;
+    }
+    if (b == e)
+    {
+        t[n] = v;
+        return;
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    update(l, b, mid, i, v);
+    update(r, mid + 1, e, i, v);
+
+    t[n] = min(t[l], t[r]);
+}
+
+ll query(int n, int b, int e, int i, int j)
+{
+    if (e < i || j < b)
+    {
+        return 1000000009;
+    }
+
+    if (b >= i && e <= j)
+    {
+        return t[n];
+    }
+
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    return min(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
+}
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+
+    build(1, 1, n);
+    while (m--)
+    {
+        int t;
+        cin >> t;
+        if (t == 1)
+        {
+            int idx, v;
+            cin >> idx >> v;
+            idx++;
+            update(1, 1, n, idx, v);
+        }
+        else
+        {
+            int l, r;
+            cin >> l >> r;
+            l++;
+            ll ans = query(1, 1, n, l, r);
+            cout << ans << endl;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    tc = 1;
+    // cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
